47c9dab29b98e46bfb2a0f031ff06fe961751635
Joerg Hochwald <jhochwald@users.noreply.github.com>
2026-05-09 13:36:43 +0200

    Very simple top like tool

---
+++ Windows11/SimpleTop.ps1
@@ -0,0 +1,8 @@
+﻿While (1) 
+{
+   Get-Process | Sort-Object -Descending -Property cpu | Select-Object -First 30
+   Start-Sleep -Seconds 2
+   Clear-Host
+   Write-Output -InputObject 'Handles  NPM(K)    PM(K)      WS(K) VM(M)   CPU(s)     Id ProcessName'
+   Write-Output -InputObject '-------  ------    -----      ----- -----   ------     -- -----------'
+}
